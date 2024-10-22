b8331bf2d28b747fbfa92911e6a14e2ca42dc121
Kemilsaadouni <kemil.saadouni@epitech.digital>
2024-10-22 11:51:54 +0200

    Add a legend to the chart

--- src/components/MoodGraph.jsx
+++ src/components/MoodGraph.jsx
@@ -27,17 +27,14 @@ ChartJS.register(
 const MoodGraph = ({ dailyAverages }) => {
   const chartRef = useRef(null);
 
-  // Fonction pour générer un dégradé de couleur
   const createGradient = (context, chartArea) => {
     const gradient = context.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
-    // Ajout des points de couleur pour le dégradé
-    gradient.addColorStop(0, 'blue');  // Valeur basse
-    gradient.addColorStop(0.5, 'yellow');  // Valeur intermédiaire
-    gradient.addColorStop(1, 'red');  // Valeur haute
+    gradient.addColorStop(0, 'blue');
+    gradient.addColorStop(0.5, 'yellow');
+    gradient.addColorStop(1, 'red');
     return gradient;
   };
 
-  // Configuration des données et du dégradé
   const data = {
     labels: dailyAverages.map(item => item.date),
     datasets: [
@@ -48,16 +45,16 @@ const MoodGraph = ({ dailyAverages }) => {
         borderColor: 'rgba(75, 192, 192, 1)',
         backgroundColor: (context) => {
           const chart = chartRef.current;
-          if (!chart) return 'rgba(75, 192, 192, 1)';  // Couleur par défaut
+          if (!chart) return 'rgba(75, 192, 192, 1)';
           const { ctx, chartArea } = chart;
           return createGradient(ctx, chartArea);
-        },  // Couleur du remplissage
+        },
       },
     ],
   };
 
   const options = {
-    maintainAspectRatio: false,  // Désactiver l'aspect ratio pour permettre au graphique de s'étendre
+    maintainAspectRatio: true,  // Désactiver l'aspect ratio pour permettre au graphique de s'étendre
     layout: {
       padding: {
         bottom: 50,  // Ajouter du padding en bas du graphique
@@ -73,17 +70,29 @@ const MoodGraph = ({ dailyAverages }) => {
       },
       y: {
         beginAtZero: true,
+        min: 1.0,
         max: 5.0,
       },
       marginBottom:'200px'
     },
+    plugins: {
+      legend: {
+        display: false,  // Désactiver la légende
+      },
+    },
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', padding: '10px'}}>
-      <div style={{ width: '90%', maxWidth: '900px', height: window.innerWidth < 768 ? '300px' : '450px', padding: '30px', backgroundColor: '#fff', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}}>
-        <h4 style={{ marginBottom: '10px' }}>Moyenne du moral des employés lors des 7 derniers jours</h4>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+      <div style={{ width: '90%', maxWidth: '900px', padding: '30px', backgroundColor: '#fff', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}}>
+        <h3 style={{ marginBottom: '10px' }}>Moyenne du moral des employés lors des 7 derniers jours</h3>
         <Line ref={chartRef} data={data} options={options} />
+        <div style={{ display: 'inline-block', width: '300px', height: '10px', background: 'linear-gradient(to right, blue, yellow, red)' }}></div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px', margin: '5px auto' }}>
+          <h5>En colère</h5>
+          <h5>Neutre</h5>
+          <h5>Très heureux</h5>
+        </div>
       </div>
     </div>
   );
