03fa77c2bc64fa4f6041cadd539d236f6c0e562f
Kemilsaadouni <kemil.saadouni@epitech.digital>
2024-10-22 13:06:32 +0200

    change design of MoodGraph

--- src/components/MoodGraph.jsx
+++ src/components/MoodGraph.jsx
@@ -54,7 +54,7 @@ const MoodGraph = ({ dailyAverages }) => {
   };
 
   const options = {
-    maintainAspectRatio: true,  // Désactiver l'aspect ratio pour permettre au graphique de s'étendre
+    maintainAspectRatio: false,  // Désactiver l'aspect ratio pour permettre au graphique de s'étendre
     layout: {
       padding: {
         bottom: 50,  // Ajouter du padding en bas du graphique
@@ -83,15 +83,17 @@ const MoodGraph = ({ dailyAverages }) => {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-      <div style={{ width: '90%', maxWidth: '900px', padding: '30px', backgroundColor: '#fff', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', padding: '10px'}}>
+      <div style={{ width: '90%', maxWidth: '900px', height: window.innerWidth < 768 ? '350px' : '450px', padding: '30px', backgroundColor: '#fff', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}}>
         <h3 style={{ marginBottom: '10px' }}>Moyenne du moral des employés lors des 7 derniers jours</h3>
         <Line ref={chartRef} data={data} options={options} />
-        <div style={{ display: 'inline-block', width: '300px', height: '10px', background: 'linear-gradient(to right, blue, yellow, red)' }}></div>
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px', margin: '5px auto' }}>
-          <h5>En colère</h5>
-          <h5>Neutre</h5>
-          <h5>Très heureux</h5>
+        <div style={{backgroundColor: '#fff', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding:20}}>
+          <div style={{ display: 'inline-block', width: '300px', height: '10px', background: 'linear-gradient(to right, blue, yellow, red)'}}></div>
+          <div style={{ display: 'flex', justifyContent: 'space-between', margin: '5px auto' }}>
+            <h5>En colère</h5>
+            <h5>Neutre</h5>
+            <h5>Très heureux</h5>
+          </div>
         </div>
       </div>
     </div>
